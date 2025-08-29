0f255ee82da8e38bdfd2ad0e4ba2c2c87380ba04
Yardan <139103935+yardanshaq@users.noreply.github.com>
2025-08-29 07:03:24 +0700

    Update LandingPage.tsx

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -128,7 +128,7 @@ export default function LandingPage({ onDownload }: LandingPageProps) {
                 ) : (
                   <>
                     <Download className="w-5 h-5 mr-2" />
-                    Get Download Options
+                    Download
                   </>
                 )}
               </Button>
